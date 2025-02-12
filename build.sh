85a6c6ac8c8153fae084fcac5f2180bb69105cda
edwisanchez <eedsc99@gmail.com>
2025-02-12 00:06:22 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+# Actualizar los paquetes e instalar las dependencias necesarias
+apt-get update && apt-get install -y curl apt-transport-https gnupg
 
-# Descargar e instalar el driver ODBC 17 para SQL Server
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt update
-sudo apt install -y --no-install-recommends \
-    unixodbc unixodbc-dev odbcinst odbcinst1debian2 libodbc1 \
-    msodbcsql17 
+# Importar la clave GPG de Microsoft
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
-# Verifica que el driver esté instalado
-odbcinst -q -d
+# Registrar el repositorio de Microsoft
+curl https://packages.microsoft.com/config/debian/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-# Instala las dependencias de Python
-pip install -r requirements.txt
+# Actualizar los paquetes e instalar el controlador ODBC 18 para SQL Server
+apt-get update && ACCEPT_EULA=Y apt-get install -y msodbcsql18
+
+# Limpiar la caché de apt para reducir el tamaño de la imagen
+apt-get clean

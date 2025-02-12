a1cb78db9970aaff766d274a662652de3db6d1ba
edwisanchez <eedsc99@gmail.com>
2025-02-11 23:52:10 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y \
+#!/bin/bash
+
+# Instalar el driver ODBC 17 para SQL Server en Linux (Debian/Ubuntu)
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "$(curl -s https://packages.microsoft.com/config/ubuntu/20.04/prod.list)"
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends \
     unixodbc \
     unixodbc-dev \
-    freetds-bin \
-    freetds-dev \
-    tdsodbc
+    odbcinst \
+    msodbcsql17
+
+# Instalar dependencias de Python
+pip install -r requirements.txt
